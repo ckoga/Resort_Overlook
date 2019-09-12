853da22280a41b5b6462091df35f14acf7d84921
Colin Turing <colin.koga@gmail.com>
2019-09-12 13:28:44 -0600

    Add fetch call into index.js going to set up a branch for users to insantiate user obj

--- src/index.js
+++ src/index.js
@@ -10,4 +10,31 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+let apiRequestUser = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then((response) => response.json());
+
+let apiRequestRooms = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then((response) => response.json());
+
+let apiRequestBookings = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then((response) => response.json());
+
+let apiRequestRoomService = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices').then((response) => response.json());
+
+let combinedData = {
+  "apiRequestUser": {},
+  "apiRequestRooms":{},
+  "apiRequestBookings":{},
+  "apiRequestRoomService":{}
+};
+
+Promise.all([ apiRequestUser, apiRequestRooms, apiRequestBookings, apiRequestRoomService ]).then(function (values) {
+  combinedData["apiRequestUser"] = values[0];
+  combinedData["apiRequestRooms"] = values[1];
+  combinedData["apiRequestBookings"] = values[2];
+  combinedData["apiRequestRoomService"] = values[3];
+  console.log(combindedData)
+  return combinedData;
+});
+
+
+
